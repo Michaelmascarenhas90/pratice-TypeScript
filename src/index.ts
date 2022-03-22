10ed8602703e274913468917eb67b04d02254f17
Michaelmascarenhas90 <michaelmascarenhastrybe@gmail.com>
2022-03-22 10:52:18 -0300

    fix: instalaçao de cors e lib de tipagem cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
-const name: string = 'Michael Mascarenhas'
-
+app.use(cors())
 app.use(routes)
 
 app.listen(3333);
